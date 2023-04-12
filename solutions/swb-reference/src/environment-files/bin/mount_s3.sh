f022069180f62fe1c5e6ad200f3d7429e06a48a1
Manikandan Thangavelu <manikandan.thangavelu@relevancelab.com>
2023-04-12 14:04:33 +0530

    Feat: Use S3FS instead of Goofys for mounting

--- solutions/swb-reference/src/environment-files/bin/mount_s3.sh
+++ solutions/swb-reference/src/environment-files/bin/mount_s3.sh
@@ -72,7 +72,8 @@ do
         if [ "$s3_role_arn" == "null" ]
         then
             printf 'Mounting internal study "%s" at "%s"\n' "$study_id" "$study_dir"
-            goofys --region $region --acl "bucket-owner-full-control" "${s3_bucket}:${s3_prefix}" "$study_dir"
+            # goofys --region $region --acl "bucket-owner-full-control" "${s3_bucket}:${s3_prefix}" "$study_dir"
+            s3fs -o endpoint=$region -o default_acl="bucket-owner-full-control" "${s3_bucket}:/${s3_prefix}" "$study_dir"
         else
             bucket_region="$(printf "%s" "$mounts" | jq -r ".[$study_idx].region" -)"
             # BYOB studies have a region specified, but in case it isn't use the default region
